a939e01fcaf50908f27c85575b70a7ed31314bae
Yuli Cacho <ycachoq1@upao.edu.pe>
2026-05-24 00:24:15 -0500

    Eliminar ruta inexistente de reporte PDF

--- __manifest__.py
+++ __manifest__.py
@@ -35,7 +35,6 @@
     "views/reportes_menu_views.xml",
     "views/reporte_recepcion_dashboard_views.xml",
     "views/reporte_almacen_movimientos_views.xml",
-    "report/reporte_recepciones_pdf.xml",
 
     'data/anio_escolar_data.xml',
     'views/anio_escolar_views.xml',
